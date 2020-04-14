34afe6d21217c0e9c35f27636884eb8e211577a2
1903974884@qq.com <liqiangeastsun@163.com>
2020-04-14 10:24:52 +0800

    no message

--- BehaviorTree/Assets/SubAssets/Scripts/GameEditor/BehaviorTree/BehaviorTree/Custom/ConfigBehaviorNode.cs
+++ BehaviorTree/Assets/SubAssets/Scripts/GameEditor/BehaviorTree/BehaviorTree/Custom/ConfigBehaviorNode.cs
@@ -2,6 +2,24 @@
 using System;
 namespace BehaviorTree
 {
+    public static class IDENTIFICATION
+    {
+        // 行为-做饭
+        public const int COOKING = 11000;
+
+        // 行为-吃饭
+        public const int EAT = 11001;
+
+        // 行为-移动到目标
+        public const int MOVE = 11002;
+
+        // 行为-看电视
+        public const int WATCH_TV = 11003;
+
+        // 通用条件节点
+        public const int CONDITION_CUSTOM = 20002;
+    }
+
 
     public class ConfigBehaviorNode
     {
@@ -12,14 +30,14 @@ namespace BehaviorTree
 
 
             #region Human
-            Config<NodeActionCooking>("行为-做饭", 11000);
-            Config<NodeActionEat>("行为-吃饭", 11001);
-            Config<NodeActionMove>("行为-移动到目标", 11002);
-            Config<NodeActionWatchTV>("行为-看电视", 11003);
+            Config<NodeActionCooking>("行为-做饭", IDENTIFICATION.COOKING);
+            Config<NodeActionEat>("行为-吃饭", IDENTIFICATION.EAT);
+            Config<NodeActionMove>("行为-移动到目标", IDENTIFICATION.MOVE);
+            Config<NodeActionWatchTV>("行为-看电视", IDENTIFICATION.WATCH_TV);
             #endregion
 
 
-            Config<NodeConditionCustom>("通用条件节点", 20002);
+            Config<NodeConditionCustom>("通用条件节点", IDENTIFICATION.CONDITION_CUSTOM);
 
         }
 
